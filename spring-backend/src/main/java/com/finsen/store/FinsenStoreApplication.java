5dbe086d0c35714685dea010744ef1b46510c0e7
Arunish Yadav <arunishyadav121@gmail.com>
2026-08-26 02:10:06 +0530

    Hardcode System.setProperty overrides for PostgreSQL datasource to guarantee 100% successful Render startup

--- spring-backend/src/main/java/com/finsen/store/FinsenStoreApplication.java
+++ spring-backend/src/main/java/com/finsen/store/FinsenStoreApplication.java
@@ -9,6 +9,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class FinsenStoreApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.datasource.url", "jdbc:postgresql://ep-silent-flower-a5s0z84j.us-east-2.aws.neon.tech/neondb?sslmode=require");
+		System.setProperty("spring.datasource.username", "neondb_owner");
+		System.setProperty("spring.datasource.password", "npg_x7LQRX9gW8vJ");
+		System.setProperty("spring.datasource.driver-class-name", "org.postgresql.Driver");
+		System.setProperty("spring.jpa.database-platform", "org.hibernate.dialect.PostgreSQLDialect");
+		System.setProperty("spring.jpa.hibernate.ddl-auto", "update");
+
 		SpringApplication.run(FinsenStoreApplication.class, args);
 	}
 

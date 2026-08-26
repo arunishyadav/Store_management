af173f5db390d0e7a2f6f987a4455b8fe683dc76
Arunish Yadav <arunishyadav121@gmail.com>
2026-08-26 10:37:00 +0530

    Hardcode embedded H2 database to permanently bypass expired Render PostgreSQL database

--- spring-backend/src/main/java/com/finsen/store/FinsenStoreApplication.java
+++ spring-backend/src/main/java/com/finsen/store/FinsenStoreApplication.java
@@ -9,15 +9,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class FinsenStoreApplication {
 
 	public static void main(String[] args) {
-		String envDb = System.getenv("SPRING_DATASOURCE_URL");
-		if (envDb == null || envDb.isEmpty() || envDb.contains("dpg-d9imu4741pts73bg3t10-a")) {
-			System.setProperty("spring.datasource.url", "jdbc:h2:file:./data/finsen_db;DB_CLOSE_ON_EXIT=FALSE;AUTO_RECONNECT=TRUE");
-			System.setProperty("spring.datasource.username", "sa");
-			System.setProperty("spring.datasource.password", "");
-			System.setProperty("spring.datasource.driver-class-name", "org.h2.Driver");
-			System.setProperty("spring.jpa.database-platform", "org.hibernate.dialect.H2Dialect");
-			System.setProperty("spring.jpa.hibernate.ddl-auto", "update");
-		}
+		System.setProperty("SPRING_DATASOURCE_URL", "jdbc:h2:file:./data/finsen_db;DB_CLOSE_ON_EXIT=FALSE;AUTO_RECONNECT=TRUE");
+		System.setProperty("spring.datasource.url", "jdbc:h2:file:./data/finsen_db;DB_CLOSE_ON_EXIT=FALSE;AUTO_RECONNECT=TRUE");
+		System.setProperty("spring.datasource.username", "sa");
+		System.setProperty("spring.datasource.password", "");
+		System.setProperty("spring.datasource.driver-class-name", "org.h2.Driver");
+		System.setProperty("spring.jpa.database-platform", "org.hibernate.dialect.H2Dialect");
+		System.setProperty("spring.jpa.hibernate.ddl-auto", "update");
 
 		SpringApplication.run(FinsenStoreApplication.class, args);
 	}

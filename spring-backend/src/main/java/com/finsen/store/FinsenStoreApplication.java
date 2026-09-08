bc6b0de7645c77431ecf2fc1ec3cd8b0d096470d
Arunish Yadav <arunishyadav121@gmail.com>
2026-09-08 22:21:36 +0530

    Fix Unable to determine Dialect without JDBC metadata error

--- spring-backend/src/main/java/com/finsen/store/FinsenStoreApplication.java
+++ spring-backend/src/main/java/com/finsen/store/FinsenStoreApplication.java
@@ -26,11 +26,15 @@ public class FinsenStoreApplication {
 		System.setProperty("spring.datasource.password", neonPass);
 		System.setProperty("spring.datasource.driver-class-name", "org.postgresql.Driver");
 
-		// HikariCP Resilience & Timeout Configurations
-		System.setProperty("spring.datasource.hikari.initialization-fail-timeout", "0");
+		// Explicit Hibernate Dialect configuration to prevent "Unable to determine Dialect without JDBC metadata"
+		System.setProperty("spring.jpa.database-platform", "org.hibernate.dialect.PostgreSQLDialect");
+		System.setProperty("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+		System.setProperty("spring.jpa.properties.hibernate.temp.use_jdbc_metadata_defaults", "false");
+
+		// HikariCP Resilience Configurations
+		System.setProperty("spring.datasource.hikari.initialization-fail-timeout", "-1");
 		System.setProperty("spring.datasource.hikari.connection-timeout", "60000");
 		System.setProperty("spring.datasource.hikari.maximum-pool-size", "10");
-		System.setProperty("spring.datasource.hikari.minimum-idle", "2");
 
 		System.setProperty("spring.jpa.hibernate.ddl-auto", "update");
 
